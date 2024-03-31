4b2a4f84a7241c97dedf678ab2c14182ceaa5316
KobakhidzeDimitri1 <dimitri.kobakhidze.1@btu.edu.ge>
2024-04-01 02:29:06 +0400

    Fixed toast not closing

--- src/components/UI/Toast/Toast.js
+++ src/components/UI/Toast/Toast.js
@@ -2,14 +2,11 @@ import "./toast.css";
 
 import { MdOutlineClose } from "react-icons/md";
 
-const Toast = ({ tooltipText, setShowToast, customClasses = "" }) => {
+const Toast = ({ tooltipText, closeToast, customClasses = "" }) => {
   return (
     <div className={`toast-container ${customClasses}`}>
       <span>{tooltipText}</span>
-      <MdOutlineClose
-        className="toast-close-icon"
-        onClick={() => setShowToast(false)}
-      />
+      <MdOutlineClose className="toast-close-icon" onClick={closeToast} />
     </div>
   );
 };
